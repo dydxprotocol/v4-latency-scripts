d5e58d0b31149372548975ec84b5d5affb61b4ed
yang-dydx <117109716+yang-dydx@users.noreply.github.com>
2024-06-20 09:26:08 -0700

    increment clob pair (#7)

--- listen_to_grpc_stream.py
+++ listen_to_grpc_stream.py
@@ -24,7 +24,7 @@ from bq_helpers import create_table, BatchWriter
 # Dataset configuration
 DATASET_ID = "full_node_stream"
 TABLE_ID = "responses"
-CLOB_PAIR_IDS = range(69)
+CLOB_PAIR_IDS = range(70)
 
 # Schema, partitioning, and clustering
 SCHEMA = [
