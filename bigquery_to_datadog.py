2431cc367e0613aa7297f51e34c6fbeb0f4546b3
Justin Barnett <61020572+jusbar23@users.noreply.github.com>
2025-07-22 17:52:24 -0400

    Change from infinite loop to cronjob (#17)

--- bigquery_to_datadog.py
+++ bigquery_to_datadog.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-import time
 from logging.handlers import RotatingFileHandler
 
 import google.cloud.bigquery as bigquery
@@ -96,7 +95,6 @@ QUERIES = QUERIES[:1]
 
 DATADOG_API_KEY = config["dd_api_key"]
 DATADOG_APP_KEY = config["dd_app_key"]
-POLL_INTERVAL = 60  # 1 minute
 STATE_FILE = "last_processed_timestamps.json"
 
 # Initialize Datadog API
@@ -139,66 +137,62 @@ def monitor():
     last_processed_timestamps = load_last_processed_timestamps()
     client = bigquery.Client(project=PROJECT_ID)
 
-    while True:
-        try:
-            for query_info in QUERIES:
-                query_name = query_info["name"]
-                query = query_info["query"]
-                params = query_info["params"]
-                metric_name = query_info["metric_name"]
-                last_processed_timestamp = last_processed_timestamps.get(
-                    query_name, START_TIMESTAMP
-                )
-                results = run_query(client, query, params, last_processed_timestamp)
+    try:
+        for query_info in QUERIES:
+            query_name = query_info["name"]
+            query = query_info["query"]
+            params = query_info["params"]
+            metric_name = query_info["metric_name"]
+            last_processed_timestamp = last_processed_timestamps.get(
+                query_name, START_TIMESTAMP
+            )
+            results = run_query(client, query, params, last_processed_timestamp)
 
-                # Save the data points by server address
-                addr_to_points = {}
-                metrics = []
+            # Save the data points by server address
+            addr_to_points = {}
+            metrics = []
 
-                # loop through results
-                latest_timestamp = last_processed_timestamp
-                for row in results:
-                    timestamp = int(row["received_at"].timestamp())
+            # loop through results
+            latest_timestamp = last_processed_timestamp
+            for row in results:
+                timestamp = int(row["received_at"].timestamp())
 
-                    points = addr_to_points.get(row["server_address"], [])
-                    points.append((timestamp, [row["latency"]]))
-                    addr_to_points[row["server_address"]] = points
+                points = addr_to_points.get(row["server_address"], [])
+                points.append((timestamp, [row["latency"]]))
+                addr_to_points[row["server_address"]] = points
 
-                    latest_timestamp = row["received_at"]
-                    tags = [
-                        f"server_address:{row['server_address']}",
-                        f"service_name:v4-latency-scripts",
-                        "environment:mainnet",
-                    ]
-                    metric = {
-                        "metric": metric_name,
-                        "points": [(timestamp, row["latency"])],
-                        "tags": tags,
-                    }
-                    metrics.append(metric)
+                latest_timestamp = row["received_at"]
+                tags = [
+                    f"server_address:{row['server_address']}",
+                    f"service_name:v4-latency-scripts",
+                    "environment:mainnet",
+                ]
+                metric = {
+                    "metric": metric_name,
+                    "points": [(timestamp, row["latency"])],
+                    "tags": tags,
+                }
+                metrics.append(metric)
 
-                # Send one distribution metric per server address
-                for server_addr, points in addr_to_points.items():
-                    tags = [
-                        f"server_address:{server_addr}",
-                        "service_name:v4-latency-scripts",
-                        "environment:test",
-                    ]
-                    logging.info(f"Sending {len(points)} dist points for {server_addr} to Datadog")
-                    api.Distribution.send(metric=metric_name + "_dist", points=points, tags=tags)
+            # Send one distribution metric per server address
+            for server_addr, points in addr_to_points.items():
+                tags = [
+                    f"server_address:{server_addr}",
+                    "service_name:v4-latency-scripts",
+                    "environment:test",
+                ]
+                logging.info(f"Sending {len(points)} dist points for {server_addr} to Datadog")
+                api.Distribution.send(metric=metric_name + "_dist", points=points, tags=tags)
 
-                if metrics:
-                    logging.info(f"Sending {len(metrics)} metrics to Datadog")
-                    api.Metric.send(metrics)
+            if metrics:
+                logging.info(f"Sending {len(metrics)} metrics to Datadog")
+                api.Metric.send(metrics)
 
-                last_processed_timestamps[query_name] = str(latest_timestamp)
-            save_last_processed_timestamps(last_processed_timestamps)
+            last_processed_timestamps[query_name] = str(latest_timestamp)
+        save_last_processed_timestamps(last_processed_timestamps)
 
-            logging.info(f"Sleeping for {POLL_INTERVAL} seconds")
-            time.sleep(POLL_INTERVAL)
-        except Exception as e:
-            logging.error(f"An error occurred: {e}")
-            time.sleep(POLL_INTERVAL)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
